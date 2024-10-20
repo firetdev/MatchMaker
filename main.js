2e42fc78f5a8d65231d9a2cb340c9c3457ad461c
flesheatindragon <futuregamesstore@gmail.com>
2024-10-20 12:58:08 -0500

    Add image select ability

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 var image1 = document.getElementById("image1");
 var image1display = document.getElementById("image1display");
+image1.addEventListener("change", e => {
+    var file = e.target.files[0];
+    var reader = new FileReader();
+    reader.onload = (event) => {
+        image1display.src = event.target.result;
+    };
+    reader.readAsDataURL(file);
+});
